a54602f7ec90da66245e9ddf8d90bb1bc1346d81
Jherod Brendon <38527453+DoomGuy005@users.noreply.github.com>
2022-10-10 15:48:38 -0300

    alguns ajustes no remover

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 import static java.lang.System.out;
 
 public class Main {
-    public static void main(String[] args) {
-        ArvoreBinariaPesquisa arvore = new ArvoreBinariaPesquisa(20);
-        arvore.inserir(10);
-        arvore.inserir(21);
-        arvore.inserir(16);
-        arvore.inserir(9);
-        arvore.inserir(9.5F);
-        arvore.inserir(9.4F);
-        arvore.inserir(9.6F);
-        arvore.inserir(17);
-        out.println(arvore);
-        arvore.remover(10);
-        out.println(arvore);
-        arvore.pesquisar(9f);
-        arvore.pesquisar(9.6f);
-    }
+	public static void main(String[] args) {
+		ArvoreBinariaPesquisa arvore = new ArvoreBinariaPesquisa(20);
+		arvore.inserir(10);
+		arvore.inserir(21);
+		arvore.inserir(16);
+		arvore.inserir(9);
+		arvore.inserir(9.5F);
+		arvore.inserir(9.4F);
+		arvore.inserir(9.6F);
+		arvore.inserir(17);
+		out.println(arvore);
+		arvore.remover(10);
+		out.println(arvore);
+		arvore.remover(9.5f);
+		out.println(arvore);
+		arvore.remover(9.6f);
+		out.println(arvore);
+		arvore.inserir(22);
+		out.println(arvore);
+		arvore.inserir(21.5f);
+		out.println(arvore);
+		arvore.inserir(21.6f);
+		out.println(arvore);
+		arvore.pesquisar(21.6f);
+		arvore.remover(22);
+		out.println(arvore);
+	}
 }
